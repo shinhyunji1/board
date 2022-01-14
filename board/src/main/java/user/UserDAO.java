456a1aacd96b8c27e971ae39477540da5084e2e0
shinhyunji1 <shji0814@gmail.com>
2022-01-14 15:11:29 +0900

    .

--- board/src/main/java/user/UserDAO.java
+++ board/src/main/java/user/UserDAO.java
@@ -6,9 +6,13 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class UserDAO {
-  private Connection conn;
-  private PreparedStatement pstmt;
-  private ResultSet rs;
+  private Connection conn;//자바와 데이터베이스를 연결
+  private PreparedStatement pstmt;//쿼리문 대기 및 설정
+  private ResultSet rs;//결과값 받아오기
+
+  //기본 생성자
+  //UserDAO가 실행되면 자동으로 생성되는 부분
+  //메소드마다 반복되는 코드를 이곳에 넣으면 코드가 간소화된다
 
   public UserDAO() {
     try {
@@ -24,11 +28,11 @@ public class UserDAO {
   }
 
   public int login(String id, String password) {
-    String sql=" select password from user where id = ? ";
+    String sql=" select password from user where id = ? ";//sql쿼리문을 대기 시킨다
     try {
       pstmt = conn.prepareStatement(sql);
-      pstmt.setString(1, id);
-      rs = pstmt.executeQuery();
+      pstmt.setString(1, id);//첫번째 '?'에 매개변수로 받아온 'iD'를 대입
+      rs = pstmt.executeQuery();//쿼리를 실행한 결과를 rs에 저장
       if (rs.next()) {
         if (rs.getString(1).equals(password)) {
           return 1; // 로그인 성공.
